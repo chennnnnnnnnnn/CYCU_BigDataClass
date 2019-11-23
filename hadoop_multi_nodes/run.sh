0663ae16e192df5a84aac7ea7fed0a4ec4c430db
chennnnnnnnnnn <50040010+chennnnnnnnnnn@users.noreply.github.com>
2019-11-23 15:17:18 +0800

    Update run.sh

--- hadoop_multi_nodes/run.sh
+++ hadoop_multi_nodes/run.sh
@@ -6,12 +6,18 @@ sudo adduser --disabled-password  --gecos "" --ingroup hadoop hduser
 # wget http://172.18.39.68/os/hadoop-2.10.0.tar.gz
 wget http://172.20.10.2/jdk-8u121-linux-x64.tar.gz
 wget http://172.20.10.2/hadoop-2.8.5.tar.gz
-sudo tar zxvf hadoop-2.10.0.tar.gz -C /usr/local/
-sudo tar zxvf jdk-8u231-linux-x64.tar.gz -C /usr/local/
-sudo ln -s /usr/local/hadoop-2.10.0/ /usr/local/hadoop
-sudo ln -s /usr/local/jdk1.8.0_231/ /usr/local/jdk
-sudo chown -R hduser:hadoop /usr/local/hadoop-2.10.0/
-sudo chown -R root:root /usr/local/jdk1.8.0_231
+# sudo tar zxvf hadoop-2.10.0.tar.gz -C /usr/local/
+sudo tar zxvf hadoop-hadoop-2.8.5.tar.gz -C /usr/local/
+#vsudo tar zxvf jdk-8u231-linux-x64.tar.gz -C /usr/local/
+sudo tar zxvf jdk-8u121-linux-x64.tar.gz -C /usr/local/
+# sudo ln -s /usr/local/hadoop-2.10.0/ /usr/local/hadoop
+sudo ln -s /usr/local/hadoop-2.8.5/ /usr/local/hadoop
+# sudo ln -s /usr/local/jdk1.8.0_231/ /usr/local/jdk
+sudo ln -s /usr/local/jdk1.8.0_121/ /usr/local/jdk
+# sudo chown -R hduser:hadoop /usr/local/hadoop-2.10.0/
+sudo chown -R hduser:hadoop /usr/local/hadoop-2.8.5/
+# sudo chown -R root:root /usr/local/jdk1.8.0_231
+sudo chown -R root:root /usr/local/jdk1.8.0_121
 
 
 sudo git clone https://github.com/fychao/CYCU_BigDataClass.git /usr/local/hadoop/etc/CYCU_BigDataClass
